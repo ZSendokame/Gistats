a4fb71bae9852b3d2b88db47f7bbd51c99aaed11
Sendo kame <sendo@sendokame.com>
2022-09-22 04:50:40 -0300

    Source code.

--- gistats/main.py
+++ gistats/main.py
@@ -5,10 +5,11 @@ import requests
 from .util import sep
 
 class Login:
-    def __init__(self, token, gist_id, filename='gistats.ini'):
+    def __init__(self, name, token, gist_id, filename='gistats.ini'):
         self.token = token
         self.gist_id = gist_id
         self.filename = filename
+        self.name = name
 
     def update(self, statistics, separator='.', until=15):
         content = sep(statistics, separator, until)
@@ -21,7 +22,7 @@ class Login:
                     self.filename: {'content': content}
                 }
             },
-            auth=('ZSendokme', self.token)
+            auth=(self.name, self.token)
         )
 
         return response.status_code
